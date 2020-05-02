cd797a425ca8fd2dc0823f93f5ce367ad9c82141
Phool Dx <59568224+Phool-Frontend@users.noreply.github.com>
2020-05-01 20:55:22 -0500

    Metiendo la conexion de heroku

--- clases/Conexion.php
+++ clases/Conexion.php
@@ -1,10 +1,13 @@
 <?php
+//$conn = new mysqli('       HOST         ','  USUARIO     ','CONTRASEÑA','   NOMBRE BD ');**** Con este me dio en CleverCloud && Heroku
+
+
         class Conectar{
             public function conexion(){
-                $servidor   =   "localhost";
-                $usuario    =   "root";
-                $password   =   "";
-                $base       =   "gestor";
+                $servidor   =   "bwqhowfcs15xyiy11glz-mysql.services.clever-cloud.com";
+                $usuario    =   "usjpnyayzstwxapy";
+                $password   =   "7DZkhooohJnjo8Xf4u4p";
+                $base       =   "bwqhowfcs15xyiy11glz";
             
                 $conexion = mysqli_connect(
                                                 $servidor,
