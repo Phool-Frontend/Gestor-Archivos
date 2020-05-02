2397bf32c2c5f787d377351e837d0b5bc1cdbfb5
Phool Dx <59568224+Phool-Frontend@users.noreply.github.com>
2020-05-02 12:15:14 -0500

    ico

--- index.php
+++ index.php
@@ -5,7 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <link rel="stylesheet" href="css/login.css">
-    <link rel="stylesheet" href="librerias/bootstrap4/bootstrap.min.css"> 
+    <link rel="stylesheet" href="librerias/bootstrap4/bootstrap.min.css">    	
+    <link  rel="icon"   href="img/FacultadAutodidacta.ico" type="image/ico" />
+
 </head>
 <body>              
             <div class="wrapper fadeInDown">
